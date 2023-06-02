6ef97c2ef37bea4b6cd3c51579cf8e749f37c76e
Vidit Keshari <95676105+Vidit-Keshari@users.noreply.github.com>
2023-06-02 19:56:22 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+var text_1 = "WELCOME to AI DJ webapp where you can play DJ using your hands in front of your camera";
+var text_2 = text_1 + " Note:- Put your left hand in front of camera to play birthday song and right hand to play Harry Potter tittle song";
+function welcome() {
+    window.alert(text_1);
+    console.log(text_1);
+}
+
+function wel_note () {
+    window.alert(text_2);
+    console.log(text_2);
+}
